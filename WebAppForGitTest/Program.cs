e60e65c213e6f4302b1c444305a65b469300b805
Roman Perekrestov <mr.perekrestov@gmail.com>
2019-08-25 13:47:06 +0200

    asteriks addit to web host builder

--- WebAppForGitTest/Program.cs
+++ WebAppForGitTest/Program.cs
@@ -19,6 +19,8 @@ namespace WebAppForGitTest
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseUrls("http://*:5000")
+                .UseKestrel();
     }
 }
